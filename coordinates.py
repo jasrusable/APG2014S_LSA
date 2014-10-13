06109252ff331431946b02a5fe28de087dee75ac
Jason Russell <jasrusable@users.noreply.github.com>
2014-10-13 09:47:42 +0200

    omg

--- coordinates.py
+++ coordinates.py
@@ -10,6 +10,6 @@ class Coordinate (object):
 	def __init__(self, type_=None, name=None, y=None, x=None, line_from_file=None):
 		self.name = name
 		self.type_ = type_
-		self.y = x
+		self.y = y
 		self.x = x
 		self.line_from_file = line_from_file
