4c96405b7a1e61fb8b8cee8640aea6c331242069
Jason Russell <jasrusable@users.noreply.github.com>
2014-10-14 09:47:40 +0200

    distances

--- main.py
+++ main.py
@@ -56,6 +56,7 @@ observations = read_observations_file()
 set_up_points = get_set_up_points(observations)
 number_of_set_ups = len(set_up_points)
 
+
 A = numpy.matrix([
 		[0, 0] + [0] * number_of_set_ups,
     ])
@@ -73,40 +74,55 @@ P = numpy.delete(P, (0), axis=0)
 
 i = 0
 for observation in observations:
-	P = numpy.vstack([P, [0] * len(observations) ])
-	P.itemset((i, i), 1)
-	i = i + 1
+        P = numpy.vstack([P, [0] * len(observations)])
+        if observation.type_ == 'direction':    
+                P.itemset((i, i), 1)
+        if observation.type_ == 'distance':    
+                P.itemset((i, i), 15625)
+        i = i + 1
 
 observation_number = 1
 n = 0
 for set_up_point_name in set_up_points:
-	for observation in observations:
-		if observation.from_point.name == set_up_point_name:
-			row = [0, 0] + [0] * number_of_set_ups
-			if observation.type_ == 'direction':
-				if observation.to_point.type_ == 'provisional':
-					d = get_distance(observation.to_point, observation.from_point)
-					y = 206264.8 * (observation.to_point.x - observation.from_point.x) / d**2
-					x = -206264.8 * (observation.to_point.y - observation.from_point.y) / d**2
-					row[0], row[1] = y, x
-					row[1 + observation_number] = -1
-					A = numpy.vstack([A, row])
-				if observation.to_point.type_ == 'fixed':
-					n = n + 1
-					row[1 + observation_number] = -1
-					A = numpy.vstack([A, row])
-				observed = observation.value
-				calculated = get_direction(observation.from_point, observation.to_point)
-				l = numpy.vstack([l, (math.degrees(observed-calculated)*3600)])
-			if observation.type_ == 'distance':
+        for observation in observations:
+                if observation.from_point.name == set_up_point_name:
+                        row = [0, 0] + [0] * number_of_set_ups
+                        observed = None
+                        calculated = None
+                        if observation.type_ == 'direction':
+                                observed = observation.value
+                                calculated = get_direction(observation.from_point, observation.to_point)
+                                if observation.to_point.type_ == 'provisional':
+                                        d = get_distance(observation.to_point, observation.from_point)
+                                        y = 206264.8 * (observation.to_point.x - observation.from_point.x) / d**2
+                                        x = -206264.8 * (observation.to_point.y - observation.from_point.y) / d**2
+                                        row[0], row[1] = y, x
+                                        row[1 + observation_number] = -1
+                                        A = numpy.vstack([A, row])
+                                if observation.to_point.type_ == 'fixed':
+                                        n = n + 1
+                                        row[1 + observation_number] = -1
+                                        A = numpy.vstack([A, row])
+                                oc = (math.degrees(observed-calculated)*3600)
+                                l = numpy.vstack([l, oc])
 				
+        observation_number = observation_number + 1
 
-	observation_number = observation_number + 1
-
+for observation in observations:
+        row = [0, 0] + [0] * number_of_set_ups
+        if observation.type_ == 'distance':
+                distance = observation.value
+                y = -(observation.to_point.y - observation.from_point.y) / distance
+                x = -(observation.to_point.x - observation.from_point.x) / distance
+                row[0], row[1] = y, x
+                A = numpy.vstack([A, row])
+                observed = distance
+                calculated = get_distance(observation.to_point, observation.from_point)
+                oc = observed - calculated
+                l = numpy.vstack([l, oc])
 
 X = ((A.T) * P * A).I * (A.T) * P * l
 V = (A * X) - l
-
 variance_factor = (V.T * V) / (n - (2 + number_of_set_ups) )
 
 print X
